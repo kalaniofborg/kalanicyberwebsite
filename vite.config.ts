4aefb6035c21afb7a9a49f99dbd8e42c331ac7e2
kalani <cyberkalani@gmail.com>
2026-02-22 01:27:36 -0600

    Engage warp drive

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-   base: "/kalanicyberwebsite/",
   server: {
     host: "::",
     port: 8080,
